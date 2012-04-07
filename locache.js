d6c312201746507a6804b1d11c86d84fabd1bf16
Dougal Matthews <dougal85@gmail.com>
2012-04-07 12:53:32 +0100

    Finished the first draft of the annotated source.

--- locache.js
+++ locache.js
@@ -28,8 +28,8 @@
     locache.VERSION = "1.0.0";
 
     // Boolean value that determines if they browser supports localStorage or
-    // not. This is based on the Modernizr implementation that can be found in
-    // [the Modernizr GitHub repository.](https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731)
+    // not. This is based on the Modernizr implementation that can be found
+    // in [the Modernizr GitHub repository.](https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731)
     locache.supportsLocalStorage = (function() {
 
         try {
@@ -39,8 +39,9 @@
             localStorage.setItem(test_val, test_val);
             localStorage.getItem(test_val);
             localStorage.removeItem(test_val);
-            // If any of the checks fail, an exception will be raised. At that
-            // point we can flag the browser as not supporting localStorage.
+            // If any of the checks fail, an exception will be raised. At
+            // that point we can flag the browser as not supporting
+            // localStorage.
             return true;
         } catch(e) {
             return false;
@@ -153,8 +154,8 @@
         // If localStorage isn't supported perform a no-op.
         if (!this.supportsLocalStorage) return null;
 
-        // If the value has expired, before returning null remove the key from
-        // localStorage to free up the space.
+        // If the value has expired, before returning null remove the key
+        // from localStorage to free up the space.
         if (this.hasExpired(key)){
             this.remove(this.key(key));
             return null;
@@ -165,9 +166,9 @@
 
         // After we have the value back, check its truthy and then attempt to
         // parse the JSON. If the JSON parsing fails, return null. This could
-        // be handled better but its hard to know what to do here? We only set
-        // JSON and thus we expect JSON but we don't want to delete values
-        // that must have come from another source.
+        // be handled better but its hard to know what to do here? We only
+        // set JSON and thus we expect JSON but we don't want to delete
+        // values that must have come from another source.
         if (value){
             try{
                 return JSON.parse(value);
@@ -197,7 +198,11 @@
 
     };
 
-    // Given a key name, fetch it, increment the value and store it again.
+    // Given a key name, fetch it, increment the value and store it again. If
+    // the counter hasn't be initialised yet, set it to zero and then perform
+    // the increment. The fetched value is always parsed as an int to make
+    // sure the increment will work - this means if a non-int was stored, it
+    // will be converted first and thus reset the counter to zero.
     locache.incr = function(key){
 
         // If localStorage isn't supported perform a no-op.
@@ -213,6 +218,7 @@
 
     };
 
+    // Exactly the same as the incr function, but with a decrementing value.
     locache.decr = function(key){
 
         // If localStorage isn't supported perform a no-op.
@@ -228,12 +234,17 @@
 
     };
 
+    // Given a properties object, in the form of {key: value, key:value} set
+    // multiple keys.
     locache.setMany = function(properties){
 
         // If localStorage isn't supported perform a no-op.
         if (!this.supportsLocalStorage) return;
 
+        // Iterate through all the object properties.
         for (var key in properties) {
+            // Ignore any inherited properties, by making sure they are in
+            // the given objecct.
             if (properties.hasOwnProperty(key)) {
                 locache.set(key, properties[key]);
             }
@@ -241,11 +252,16 @@
 
     };
 
+    // Given an array of keys, return an array of values. If values don't
+    // exist, null will be in their place.
     locache.getMany = function(keys){
 
         var results = [];
 
         for (var i=0; i < keys.length; i++){
+            // To ensure that the correct structure is returned, if
+            // localStorage isn't supported return an array of null values
+            // with the correct length.
             if (this.supportsLocalStorage){
                 results.push(this.get(keys[i]));
             } else {
@@ -257,6 +273,7 @@
 
     };
 
+    // Given an array of keys, remove all of them from the cache.
     locache.removeMany = function(keys){
 
         // If localStorage isn't supported perform a no-op.
@@ -268,6 +285,8 @@
 
     };
 
+    // Delete all stored values from the cache. This method will only remove
+    // values added to localStorage with the locache prefix in the key.
     locache.flush = function(){
 
         // If localStorage isn't supported perform a no-op.
@@ -276,6 +295,8 @@
         var length = this.storage.length();
         var prefix = this.cachePrefix;
 
+        // Iteratate through all the keys stored in localStorage - if the key
+        // starts with the prefix cache prefix, then remove that key.
         for (var i=0; i < length; i++) {
             var key = this.storage.key(i);
             if (key && key.indexOf(prefix) === 0) this.storage.remove(key);
@@ -283,6 +304,8 @@
 
     };
 
+    // Return the number of cache values stored in localStorage. This only
+    // calculates the values stored by locache.
     locache.length = function(){
 
         // If localStorage isn't supported perform a no-op and return zero.
@@ -300,6 +323,10 @@
 
     };
 
+    // A cleanup utility method to remove expired keys. Iterate through all
+    // the keys stored inlocalStorage. If they key is a locache key (it has
+    // the prefix) then check to see if the key has expired. If it has,
+    // remove the key from the cache.
     locache.cleanup = function(){
 
         // If localStorage isn't supported perform a no-op.
@@ -310,6 +337,10 @@
 
         for (var i=0; i < length; i++) {
             var key = this.storage.key(i);
+            // If the key matches, remove the prefix to get the original key
+            // and then make use of the normal remove method that will clean
+            // up the cache value key pair and the cache epiration time key
+            // pair.
             if (key && key.indexOf(prefix) === 0){
                 var actualKey = key.substring(prefix.length, key.length);
                 if (this.hasExpired(actualKey)){
